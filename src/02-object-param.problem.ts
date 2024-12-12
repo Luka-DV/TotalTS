1dedc32c293cb342aaeb8a326286fae349a82837
Luka-DV <luka.guva@gmail.com>
2024-12-12 11:05:40 +0100

    add solution 02

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+type NumType = {
+  first: number;
+  second: number;
+}
+
+export const addTwoNumbers = (params: NumType) => {
   return params.first + params.second;
 };
 
