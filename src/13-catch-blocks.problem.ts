5657c90813277f9f47b4b727340fdc30bf30bc62
Luka-DV <luka.guva@gmail.com>
2024-12-17 11:50:21 +0100

    add solution 13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -5,9 +5,23 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
     if (state === "fail") {
       throw new Error("Failure!");
     }
-  } catch (e) {
+  // } catch (error: unknown) { // error: any is not good practice
+  //   // some code that handles the error
+  //   if (typeof error === "string") {
+  //     // handle string error
+  //   } else if (error instanceof Error) {
+  //     // handle Error object
+  //   } else {
+  //     // handle other types of errors
+  //   }
+  // }
+
+} catch (e) {
+  if (e instanceof Error) {
     return e.message;
   }
+}
+
 };
 
 it("Should return the message when it fails", () => {
