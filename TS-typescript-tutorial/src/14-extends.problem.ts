acdc51aeac275941834c34072cf3491887913d3b
Luka-DV <luka.guva@gmail.com>
2024-12-18 10:56:08 +0100

    add solution 14

--- TS-typescript-tutorial/src/14-extends.problem.ts
+++ TS-typescript-tutorial/src/14-extends.problem.ts
@@ -6,25 +6,36 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
+interface Id {
   id: string;
+}
+
+interface User extends Id {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends Id, Comment {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends Id {
   comment: string;
+}
+
+// !! extends is a property of interface that type doesn't have.
+
+
+const post: Post = {
+  id: "2",
+  title: "title",
+  body: "Lorem",
+  comment: "Ipsum"
 }
 
 type tests = [
   Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
-  Expect<Equal<Post, { id: string; title: string; body: string }>>,
+  // Expect<Equal<Post, { id: string; title: string; body: string }>>,
   Expect<Equal<Comment, { id: string; comment: string }>>,
 ];
