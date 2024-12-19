f3279a18834cc0d02f908e63af90bceccda82660
Luka-DV <luka.guva@gmail.com>
2024-12-19 15:25:48 +0100

    add solution 03

--- typescript-generics-workshop/src/01-generics-intro/03-multiple-generics.problem.ts
+++ typescript-generics-workshop/src/01-generics-intro/03-multiple-generics.problem.ts
@@ -1,12 +1,22 @@
+
+
+
+
+
+
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {
+const returnBothOfWhatIPassIn = <A, B>(a: A, b: B) => {
   return {
     a,
     b,
   };
 };
+
+//when you start using multiple generics TS gets more cautious off how it infers types - it infers just the types (string, number), not deeply ("a", 1)
+//if we wanted that we would have to write ie A extends string etc.
+
 
 it("Should return an object of the arguments you pass", () => {
   const result = returnBothOfWhatIPassIn("a", 1);
