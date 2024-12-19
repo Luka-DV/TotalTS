12d635d75de62d5ede2cb8dead4935b2160486e3
Luka-DV <luka.guva@gmail.com>
2024-12-19 14:49:00 +0100

    add workshop to main repo

---
+++ typescript-generics-workshop/src/01-generics-intro/01-return-what-i-pass-in.problem.ts
@@ -0,0 +1,17 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+const returnWhatIPassIn = <Type>(t: Type): Type => {
+  return t;
+};
+
+// or:
+// function returnWhatIPassIn<T>(t: T): T {
+//   return t
+// }
+
+const one = returnWhatIPassIn(1);
+const matt = returnWhatIPassIn("matt");
+
+type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
+
+
